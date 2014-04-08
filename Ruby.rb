00eff865210957efd147dc14935e71fb863c6ed4
Mimi <maria.bozhkova@yahoo.com>
2014-04-08 21:02:15 +0300

    Starting to use ruby

---
+++ Ruby.rb
@@ -0,0 +1,41 @@
+#!/bin/env ruby
+# encoding: utf-8
+header='<!DOCTYPE html>
+<html>
+<head>
+	<title>Maria Bozhkova</title>
+	<style>
+	    body {
+		    font-family:Comic Sans MS;
+		    color:#4674B0;
+		    /*color:rgb(70,116,136);*/
+		    background-image:url(\'../images/background.jpg\');
+		    background-size: cover
+		}
+	    h1{font-size: 72px;}
+	    span{position: absolute; bottom: 20pt; left: 10pt;}
+	    img{max-width: 70px;}
+	</style>
+</head>
+<body><h1>'
+name='Мария Божкова'
+name_en='Maria Bozhkova'
+footer='
+	</h1><p>
+		"Better to be the one who smiled than the one
+		who didn\'t smile back!"
+	</p>
+	<span>
+ 		<a href="https://www.facebook.com/violette.winderhill" target="_blank">
+ 		<img src="../images/facebook.png"></a>
+		<a href="https://instagram.com/mmrrawr" target="_blank">
+		<img src="../images/instagram.png"></a>
+		<a href="https://plus.google.com/u/0/107895210775635386987/posts" target="_blank">
+		<img src="../images/google.png"></a>
+	</span>
+</body>
+</html>'
+File.write "C:/Users/mimi/Documents/GitHub/mmrawr.github.io/bg/indexbg.html", header + name + footer
+File.write "C:/Users/mimi/Documents/GitHub/mmrawr.github.io/en/indexen.html", header + name_en + footer
+
+
